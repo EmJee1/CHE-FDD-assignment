2917702b9cc698b1d5c08bee31be4d22334302c0
EmJee <mart-janroeleveld@outlook.com>
2022-09-20 23:38:01 +0200

    🌑 Update navbar box-shadow color

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -46,7 +46,7 @@ const onToggleNavbar = () => {
   transition: all 0.3s ease-in-out;
 
   background: rgba(255, 255, 255, 0.25);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  box-shadow: 0 8px 32px 0 transparentize(colors.$black, 0.8);
   backdrop-filter: blur(8px);
 
   &.expanded {
