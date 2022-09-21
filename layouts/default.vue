0913c077637c380a11ab8348a4dfa0d26c842ed0
EmJee <mart-janroeleveld@outlook.com>
2022-09-21 17:13:32 +0200

    🧹 Remove redundant onMounted hook

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,11 @@
 <script lang="ts" setup>
 import "../styles/global.scss";
-import { onMounted, watch, useRoute, useRouter, useSlide } from "#imports";
+import { watch, useRoute, useRouter, useSlide } from "#imports";
 
 const route = useRoute();
 const router = useRouter();
 const { selectedSlide, navigateToSlide, navigateRespective } = useSlide();
 
-onMounted(() => navigateToSlide(route.path));
 watch(() => route.path, navigateToSlide, { immediate: true });
 watch(
   () => selectedSlide.value,
