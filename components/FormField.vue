65de542bda10090d20c74bd169106a9b28972986
EmJee <mart-janroeleveld@outlook.com>
2022-09-30 17:39:00 +0200

    Add basic form-field styling

--- components/FormField.vue
+++ components/FormField.vue
@@ -18,13 +18,42 @@ const onInput = (e: InputEvent) => {
 </script>
 
 <template>
-  <label class="label" :for="id">{{ label }}</label>
-  <input
-    class="input"
-    required
-    :type="type"
-    :id="id"
-    :value="modelValue"
-    @input="onInput"
-  />
+  <div class="field">
+    <label class="label" :for="id">{{ label }}</label>
+    <input
+      class="input"
+      required
+      :type="type"
+      :id="id"
+      :value="modelValue"
+      @input="onInput"
+    />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@use "../styles/colors";
+@use "../styles/misc";
+
+$elements-padding: 8px;
+
+.field {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+}
+
+.label {
+  font-weight: 600;
+}
+
+.input {
+  width: 100%;
+  padding: 12px;
+  border: 2px solid colors.$black;
+  background: colors.$white;
+  border-radius: misc.$border-radius;
+  outline-color: colors.$purple;
+}
+</style>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -2,12 +2,14 @@
 import { ref } from "#imports";
 
 const email = ref("");
+const name = ref("");
 const subject = ref("");
 
 const onSubmit = (e: SubmitEvent) => {
   // TODO: do something with the data
   console.log("Form submitted with data:", {
     email: email.value,
+    name: name.value,
     subject: subject.value,
   });
 };
@@ -17,9 +19,35 @@ const onSubmit = (e: SubmitEvent) => {
   <Slide>
     <h1>Contact</h1>
     <form @submit.prevent="onSubmit">
-      <FormField type="email" id="email" label="Email" v-model="email" />
-      <FormField type="text" id="subject" label="Onderwerp" v-model="subject" />
+      <div class="form-row">
+        <FormField type="email" id="email" label="Email" v-model="email" />
+        <FormField type="text" id="name" label="Naam" v-model="name" />
+        <FormField
+          type="text"
+          id="subject"
+          label="Onderwerp"
+          v-model="subject"
+        />
+      </div>
       <button type="submit">Versturen</button>
     </form>
   </Slide>
 </template>
+
+<style lang="scss" scoped>
+@use "../styles/breakpoints";
+
+.form-row {
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+
+  @include breakpoints.md {
+    flex-direction: row;
+  }
+
+  @include breakpoints.xl {
+    gap: 30px;
+  }
+}
+</style>
