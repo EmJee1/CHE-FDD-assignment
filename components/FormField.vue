671839319bd63640f8991c888f4dc755012ca18c
EmJee <mart-janroeleveld@outlook.com>
2022-09-30 15:57:34 +0200

    Add unstyled FormField component

---
+++ components/FormField.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+defineProps<{
+  type: "text" | "email";
+  modelValue: string;
+  label: string;
+  id: string;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string);
+}>();
+
+const onInput = (e: InputEvent) => {
+  // Casting here is fine since we know for sure that the value is from an input element
+  const value = (e.target as HTMLInputElement).value.trim();
+  emit("update:modelValue", value);
+};
+</script>
+
+<template>
+  <label class="label" :for="id">{{ label }}</label>
+  <input
+    class="input"
+    required
+    :type="type"
+    :id="id"
+    :value="modelValue"
+    @input="onInput"
+  />
+</template>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,7 +1,25 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "#imports";
+
+const email = ref("");
+const subject = ref("");
+
+const onSubmit = (e: SubmitEvent) => {
+  // TODO: do something with the data
+  console.log("Form submitted with data:", {
+    email: email.value,
+    subject: subject.value,
+  });
+};
+</script>
 
 <template>
   <Slide>
     <h1>Contact</h1>
+    <form @submit.prevent="onSubmit">
+      <FormField type="email" id="email" label="Email" v-model="email" />
+      <FormField type="text" id="subject" label="Onderwerp" v-model="subject" />
+      <button type="submit">Versturen</button>
+    </form>
   </Slide>
 </template>
