0e51608880154240823e0626e61c2b12ed4a9427
EmJee <mart-janroeleveld@outlook.com>
2022-10-03 13:43:46 +0200

    Set keep-alive in nuxt-config

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["@fortawesome/fontawesome-svg-core/styles.css"],
+  app: {
+    keepalive: true,
+  },
 });
