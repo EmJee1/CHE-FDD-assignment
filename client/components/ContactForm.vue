3909ae62643752b5c7cce0cd24d3c811c9153c6e
EmJee <mart-janroeleveld@outlook.com>
2022-10-03 17:33:09 +0200

    Consistent spacing in contact form

--- client/components/ContactForm.vue
+++ client/components/ContactForm.vue
@@ -99,28 +99,21 @@ const onSubmit = async (e: SubmitEvent) => {
 
 <style lang="scss" scoped>
 @use "../styles/breakpoints";
+@use "../styles/spacing";
 
 .form {
   display: flex;
   flex-direction: column;
-  gap: 14px;
-
-  @include breakpoints.md {
-    gap: 18px;
-  }
+  @include spacing.content-block-item-margin(gap);
 }
 
 .form-row {
   display: flex;
   flex-direction: column;
-  gap: 14px;
+  @include spacing.content-block-item-margin(gap);
 
   @include breakpoints.md {
     flex-direction: row;
-  }
-
-  @include breakpoints.xl {
-    gap: 30px;
   }
 }
 </style>
