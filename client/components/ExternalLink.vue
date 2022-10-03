9099fa46d6eccccb32c53021faeda435e9869204
EmJee <mart-janroeleveld@outlook.com>
2022-10-03 16:00:48 +0200

    Add external-link component

---
+++ client/components/ExternalLink.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+defineProps<{
+  to: string;
+}>();
+</script>
+
+<template>
+  <a class="link" target="_blank" :href="to">
+    <slot />
+  </a>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/colors";
+
+.link {
+  color: colors.$purple;
+  text-decoration: underline;
+}
+</style>
