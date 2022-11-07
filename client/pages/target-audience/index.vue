4951fc167f29c867eaea2fdacdd3ce8a5b9e2f26
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-11-07 21:01:18 +0100

    Improved reusable classes

--- client/pages/target-audience/index.vue
+++ client/pages/target-audience/index.vue
@@ -1,16 +1,16 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
+  <div class="content-page">
     <h1 class="title">Doelgroeponderzoek</h1>
-    <p>
+    <p class="content">
       Om een goede website te maken moet je eerst vaststellen wie je doelgroep
       is. Je moet erachter zien te komen wat die mensen van jouw website
       verwachten, zodat je duidelijk weet wat je uiteindelijk moet gaan
       ontwikkelen.
     </p>
-    <p>De doelgroepen heb ik in 3 categorieën verdeeld.</p>
-    <ol>
+    <p class="content">De doelgroepen heb ik in 3 categorieën verdeeld.</p>
+    <ol class="content">
       <!-- TODO: nicer list component with group clarification only shown on expand -->
       <li>Om docenten om inzicht te geven in mijn kennis over het vak FDD.</li>
       <li>Om te laten zien wat ik doe aan familie en vrienden</li>
@@ -18,15 +18,3 @@
     </ol>
   </div>
 </template>
-
-<style lang="scss" scoped>
-@use "../styles/spacing";
-
-.title {
-  @include spacing.content-block-margin(margin-bottom);
-}
-
-p {
-  @include spacing.content-block-item-margin(margin-bottom);
-}
-</style>
