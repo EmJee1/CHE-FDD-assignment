1e481854a22ea3f685870e9a811743b7c3b21ea0
EmJee <mart-janroeleveld@outlook.com>
2022-11-08 17:44:01 +0100

    Nuxt config deployment targets

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,8 +1,20 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+
+const getBaseUrl = () => {
+  const environment = process.env.DEPLOY_TARGET;
+
+  switch (environment) {
+    case "amazon":
+      return "/2223/mjroeleveld/";
+    default:
+      return "/";
+  }
+};
+
 export default defineNuxtConfig({
   css: ["@fortawesome/fontawesome-svg-core/styles.css"],
   app: {
     keepalive: true,
-    baseURL: "/",
+    baseURL: getBaseUrl(),
   },
 });
