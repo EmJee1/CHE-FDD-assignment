d1a7d74d64f4d88b8ffa43001de842069cb60986
EmJee <mart-janroeleveld@outlook.com>
2022-11-08 21:12:27 +0100

    Use correct contact function url & cors update

--- functions/src/allowed-origins.ts
+++ functions/src/allowed-origins.ts
@@ -2,6 +2,7 @@ export const getAllowedOrigins = () => {
   const allowedOrigins = [
     "https://che-fdd-assignment.web.app",
     "https://che-fdd-assignment.firebaseapp.com/",
+    "https://fdd.che-ict.nl/",
   ];
 
   if (process.env.FUNCTIONS_EMULATOR === "true") {
