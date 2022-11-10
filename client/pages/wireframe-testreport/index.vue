a6cddd55be6282a0b82e5bcde2c79fd176b118bb
EmJee <mart-janroeleveld@outlook.com>
2022-11-10 07:57:04 +0100

    Add wireframe types explanation

--- client/pages/wireframe-testreport/index.vue
+++ client/pages/wireframe-testreport/index.vue
@@ -9,12 +9,23 @@
     <ol>
       <li>
         Low fidelity is een wireframe wat een heel simpele layout schetst. Bij
-        mij is dit vergelijkbaar met het paper prototype.
+        mij is dit vergelijkbaar met het
+        <UrlLink to="/paper-prototype-sitemap-and-moodboard">
+          paper prototype
+        </UrlLink>
+        .
       </li>
       <li>
-        Medium fidelity is een wireframe met veel meer detail dan low fidelity
+        Medium fidelity is een wireframe met meer detail dan low fidelity, maar
+        het mist nog steeds visuele details. Denk daarbij aan bijvoorbeeld
+        schaduwen, kleuren en afgeronde hoekjes.
       </li>
-      <li>High fidelity</li>
+      <li>
+        High fidelity is een wireframe wat hetzelfde is, of tenminste heel erg
+        lijkt op het eindproduct. Dit wireframe is wat developers gebruiken om
+        te bouwen, mijn high fidelity wireframe (ook wel design) is te vinden op
+        <UrlLink to="/design">de design pagina</UrlLink>.
+      </li>
     </ol>
   </div>
 </template>
